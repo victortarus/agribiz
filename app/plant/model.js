5dddcbe6374af7c1d4d9f75551593aeb89d854d6
lewis nyoike <nyoikelewis@gmail.com>
2017-06-20 20:59:34 +0300

    updated model to input three images in one plant type

--- app/plant/model.js
+++ app/plant/model.js
@@ -3,6 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   alias: DS.attr(),
   image: DS.attr(),
+  image2: DS.attr(),
+  image3: DS.attr(),
   soil: DS.attr(),
   climate: DS.attr(),
   fertilizers: DS.attr(),
