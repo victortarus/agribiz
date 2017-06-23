f30ca7b14785471188dc20d5d6e779ceb014581d
shariff6 <shariff.mohammed.a@gmail.com>
2017-06-23 08:35:21 +0300

    added styling to plant tile and added weather

--- app/weather/route.js
+++ app/weather/route.js
@@ -3,9 +3,18 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
  model: function(params) {
-   var url ='api.openweathermap.org/data/2.5/forecast/daily?q=' + params.location + '&mode=xml&units=metric&cnt=7'
+   var url ='http://api.openweathermap.org/data/2.5/forecast?q='+ params.location +'us&mode=json&appid='+ 'd91ff64b66b4afe01842decf144e5a85'
    return Ember.$.getJSON(url).then(function(responseJSON) {
-     return responseJSON.results;
+     for(i=0; i<response.list.length; i++){
+       	for (x=0; x<response.list[i].weather.length;x++){
+       		var current = response.list[i].weather[x]
+       		console.log(response.list[i].weather[x])
+
+       		console.log(current.description,current.weather)
+       		console.log(response.list[i])
+       	}
+       	console.log(response.list[i].weather)
+       }
    });
   }
 });
